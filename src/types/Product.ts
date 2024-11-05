68fe335c8e65d33f9f421a9fc7f73686935008cd
Andy Uzal <andyuzal@gmail.com>
2024-11-05 16:55:34 +0000

    fix: add types

---
+++ src/types/Product.ts
@@ -0,0 +1,27 @@
+export interface Product {
+    estado: string;
+    trader: string;
+    id: string;
+    sdc: string;
+    cliente: string;
+    anunciante: string;
+    campana: string;
+    categoria: string;
+    formato: string;
+    fechaInicio: string;
+    fechaFin: string;
+    diasRestantes: string;
+    objetivoTangible: string;
+    objetivoCuantificable: string;
+    compraDeAyer: string;
+    compraTotal: string;
+    cuantoDeberiamosIr: string;
+    cuantoFaltaObjetivo: string;
+    porcentajeObjetivo: string;
+    inversionCampana: string;
+    consumoCampana: string;
+    cuantoConsumoDeberiamosIr: string;
+    consumoDeAyer: string;
+    cuantoFaltaConsumo: string;
+    porcentaje: string;
+  }
