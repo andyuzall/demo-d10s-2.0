9fe563e4508dc22c2d4cd4430358cff2b0fc61cd
Andy Uzal <andyuzal@gmail.com>
2025-02-21 15:48:59 +0100

    refactor: Delete unused helpers

--- src/helpers/calc.ts
+++
@@ -1,4 +0,0 @@
-export const calcularPorcentaje = (compraTotal: number, queCantidad: number): string => {
-    const porcentaje = (compraTotal / queCantidad) * 100;
-    return `${porcentaje.toFixed(2)}%`;
-  };
